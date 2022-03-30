0d6c1c4b3e7abb30892b8aedf22ad9a4c6efd7ac
Greg Szopian <gsz@Gregs-MacBook-Pro.local>
2022-03-30 09:45:49 +0100

    teching api from accu weather

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+const cityForm = document.querySelector('form');
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+})
---
+++ scripts/forecast.js
@@ -0,0 +1,28 @@
+const key = 'W0sBqsQQttrJHaR5puVndXmdYSnQAaGz';
+
+// get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const responce = await fetch(base + query);
+    const data = await responce.json();
+
+    
+}
+
+// get CIty information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const responce = await fetch(base + query);
+    const data = await responce.json();
+
+    return data[0];
+}; 
+
+//getCity('manchester')
+    // .then(data=>console.log(data))
+    // .catch(err=>console.log(err))
